bc19688b76fe5543d8cbea653299fe1786e74738
HumaMilisic <huma.milisic@gmail.com>
2017-02-20 23:12:55 +0100

    karta

--- AplikacijaZaZeljeznickuStanicuDRAOS2/Korak4Form.Designer.cs
+++ AplikacijaZaZeljeznickuStanicuDRAOS2/Korak4Form.Designer.cs
@@ -39,16 +39,17 @@
             this.labelM = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button7
             // 
             this.button7.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Location = new System.Drawing.Point(541, 14);
-            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Location = new System.Drawing.Point(406, 11);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(124, 50);
+            this.button7.Size = new System.Drawing.Size(93, 41);
             this.button7.TabIndex = 13;
             this.button7.Text = "Korak 4";
             this.button7.UseVisualStyleBackColor = false;
@@ -56,10 +57,10 @@
             // button6
             // 
             this.button6.Enabled = false;
-            this.button6.Location = new System.Drawing.Point(418, 14);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Location = new System.Drawing.Point(314, 11);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(124, 50);
+            this.button6.Size = new System.Drawing.Size(93, 41);
             this.button6.TabIndex = 12;
             this.button6.Text = "Korak 3";
             this.button6.UseVisualStyleBackColor = true;
@@ -67,10 +68,10 @@
             // button5
             // 
             this.button5.Enabled = false;
-            this.button5.Location = new System.Drawing.Point(297, 14);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Location = new System.Drawing.Point(223, 11);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(124, 50);
+            this.button5.Size = new System.Drawing.Size(93, 41);
             this.button5.TabIndex = 11;
             this.button5.Text = "Korak 2";
             this.button5.UseVisualStyleBackColor = true;
@@ -78,10 +79,10 @@
             // button4
             // 
             this.button4.Enabled = false;
-            this.button4.Location = new System.Drawing.Point(175, 14);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Location = new System.Drawing.Point(131, 11);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(124, 50);
+            this.button4.Size = new System.Drawing.Size(93, 41);
             this.button4.TabIndex = 10;
             this.button4.Text = "Korak 1";
             this.button4.UseVisualStyleBackColor = true;
@@ -89,10 +90,10 @@
             // button3
             // 
             this.button3.Image = global::AplikacijaZaZeljeznickuStanicuDRAOS2.Properties.Resources.helpBtn3;
-            this.button3.Location = new System.Drawing.Point(12, 14);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(9, 11);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(124, 59);
+            this.button3.Size = new System.Drawing.Size(93, 48);
             this.button3.TabIndex = 9;
             this.button3.UseVisualStyleBackColor = true;
             // 
@@ -108,10 +109,9 @@
             this.labelM.AutoSize = true;
             this.labelM.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
             this.labelM.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelM.Location = new System.Drawing.Point(978, 14);
-            this.labelM.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelM.Location = new System.Drawing.Point(734, 11);
             this.labelM.Name = "labelM";
-            this.labelM.Size = new System.Drawing.Size(472, 39);
+            this.labelM.Size = new System.Drawing.Size(377, 31);
             this.labelM.TabIndex = 21;
             this.labelM.Text = "Sarajevo 99/99/9999 99:99:99";
             // 
@@ -120,10 +120,10 @@
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button1.Location = new System.Drawing.Point(1408, 709);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(1056, 576);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 66);
+            this.button1.Size = new System.Drawing.Size(98, 54);
             this.button1.TabIndex = 20;
             this.button1.Text = "Kupi kartu";
             this.button1.UseVisualStyleBackColor = false;
@@ -132,20 +132,29 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(1271, 709);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(953, 576);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 66);
+            this.button2.Size = new System.Drawing.Size(98, 54);
             this.button2.TabIndex = 22;
             this.button2.Text = "Ponisti";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 65);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(463, 258);
+            this.richTextBox1.TabIndex = 23;
+            this.richTextBox1.Text = "";
+            // 
             // Korak4Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1553, 789);
+            this.ClientSize = new System.Drawing.Size(1165, 641);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.labelM);
             this.Controls.Add(this.button1);
@@ -155,7 +164,6 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Korak4Form";
             this.Text = "Plati kartu";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -175,5 +183,6 @@
         private System.Windows.Forms.Label labelM;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
